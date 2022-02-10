39fe83d796f6c60e2bd5f09bfe4f6a3c943e9920
Mark Chuang <markchuangth@gmail.com>
2022-02-10 11:06:40 -0500

    implemented HTTP basic auth to all admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products_count = Product.count(:all)
+    @categories_count = Category.count(:all)
   end
 end
