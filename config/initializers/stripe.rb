70e8b16a7107cc4b9819be2afe12a3dcb48a3e5c
Mark Chuang <markchuangth@gmail.com>
2022-02-10 17:59:38 -0500

    removed .env from .gitignore

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
 Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
   :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :admin_user => ENV['ADMIN_VAL_USER'],
+  :admin_password      => ENV['ADMIN_VAL_PASS']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
