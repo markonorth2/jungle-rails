eb44343266694617dd0c70b083402a96fd343084
Mark Chuang <markchuangth@gmail.com>
2022-02-13 14:41:07 -0500

    removed http_auth_concern

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,7 @@
 class Admin::DashboardController < ApplicationController
   
-  include HttpAuthConcern
+  http_basic_authenticate_with name: ENV['ADMIN_VAL_USER'], password: ENV['ADMIN_VAL_PASS']
 
-    http_basic_authenticate_with name: ENV['ADMIN_VAL_USER'], password: ENV['ADMIN_VAL_PASS']
-    
   def show
     @products_count = Product.count(:all)
     @categories_count = Category.count(:all)
